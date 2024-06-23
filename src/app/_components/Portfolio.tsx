11a10d3024cfac5906c57d49e0583ed8f2ead966
Pok13579 <151303397+Pok13579@users.noreply.github.com>
2024-06-23 14:28:53 +0700

    change port folio

--- src/app/_components/Portfolio.tsx
+++ src/app/_components/Portfolio.tsx
@@ -10,22 +10,22 @@ const Portfolio = (props: Props) => {
         <ul className="list-inside list-disc">
           <li>
             <a
-              href="https://github.com/golang/go"
+              href="https://github.com/Pok13579/miniproject-iot"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500"
             >
-              Go - Programming Language
+              Project IOT BIN
             </a>
           </li>
           <li>
             <a
-              href="https://github.com/wu-web-camp/web-profile-2024"
+              href="https://github.com/Pok13579/web_design_year2_miniproject"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500"
             >
-              web-profile-2024 - Personal Website
+              Project Webdesign
             </a>
           </li>
         </ul>
