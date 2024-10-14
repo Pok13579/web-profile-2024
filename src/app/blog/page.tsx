cd44be1723fe1eef67d237df090a3d4cbe43ec52
Pok13579 <151303397+Pok13579@users.noreply.github.com>
2024-10-14 10:39:54 +0700

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ export default function blog() {
     <main className="container bg-cy1 min-h-screen max-w-none p-24">
       <div className="container mx-auto px-40  ">
         <div className="mb-5">
-          <Link href="/" className="text-sm flex gap-2 items-center text-cy3">
+          <Link href="https://front-end-deploy-a42d7.web.app/resume/resume.html" className="text-sm flex gap-2 items-center text-cy3">
             <MoveLeft size={14} /> Profile
           </Link>
         </div>
